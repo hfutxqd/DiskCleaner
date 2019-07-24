e933169b4b6656754f98f6669c6993e71f4384c0
xuqd <xuqd@getui.com>
2019-07-24 20:04:27 +0800

    third commit.

--- core.js
+++ core.js
@@ -6,40 +6,62 @@ const format = require('date-format');
 var itemCount = 0;
 var peddingStop = false;
 
-function readDir(dir, callback) {
-    dir = path.resolve(dir);
-    if (callback && itemCount % 100 === 0) {
-        callback([itemCount, dir]);
-    }
-    let status = fs.lstatSync(dir);
+function getFile(fpath, recursive = true) {
+    fpath = path.resolve(fpath);
+    let realpath = fs.realpathSync(fpath);
+    let status = fs.lstatSync(realpath);
     if (status.isFile()) {
         return {
-            path: dir,
-            name: path.basename(dir),
+            path: fpath,
+            name: path.basename(fpath),
             type: "file",
             mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
             size: status.size
         }
     } else if (status.isSymbolicLink()) {
+        if (recursive) {
+            return getFile(fpath);
+        } else {
+            return {
+                path: fpath,
+                name: path.basename(fpath),
+                type: "link",
+                mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
+                size: status.size
+            }
+        }
+    } else if (status.isDirectory()) {
         return {
-            path: dir,
-            name: path.basename(dir),
-            type: "link",
+            path: fpath,
+            name: path.basename(fpath),
+            type: "dir",
+            mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
+            size: status.size
+        }
+    } else {
+        return {
+            path: fpath,
+            name: path.basename(fpath),
+            type: "other",
             mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
             size: status.size
         }
     }
+}
+
+function readDir(dir, callback) {
+    dir = path.resolve(dir);
+    if (callback && itemCount % 100 === 0) {
+        callback([itemCount, dir]);
+    }
+    let fileTree = getFile(dir, false);
+    if (fileTree.type != 'dir') {
+        return fileTree;
+    }
     let count = 0;
     let files = fs.readdirSync(dir);
-    let fileTree = {
-        path: dir,
-        name: path.basename(dir),
-        type: "dir",
-        mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
-        subs: [],
-        size: 0,
-        count: 0
-    }
+    fileTree.subs = [];
+    fileTree.count = 0;
     if (peddingStop) {
         return fileTree;
     }
@@ -72,6 +94,26 @@ function readDir(dir, callback) {
     return fileTree;
 }
 
+function rm(fpath, callback) {
+    if (callback) {
+        callback(fpath);
+    }
+    let status = fs.lstatSync(fpath);
+    if (status.isFile() || status.isSymbolicLink()) {
+        console.log('rm -> ' + fpath);
+        fs.unlinkSync(fpath);
+        return fpath;
+    } else if (status.isDirectory()) {
+        let files = fs.readdirSync(fpath);
+        for (let i = 0; i < files.length; i++) {
+            rm(path.resolve(fpath, files[i]), callback);
+        }
+        console.log('rm -> ' + fpath);
+        fs.rmdirSync(fpath);
+        return fpath;
+    }
+}
+
 module.exports = {
     scanDir: (dir, callback) => {
         itemCount = 0;
@@ -81,49 +123,23 @@ module.exports = {
     stop: () => {
         peddingStop = true;
     },
-    rm: (file) => {
-        
+    rm: (file, callback) => {
+        return rm(file, callback);
     },
     ls: (dir) => {
-        let files = fs.readdirSync(dir);
+        let realdir = fs.realpathSync(dir);
+        console.log('realpath=' + realdir);
+        let files = fs.readdirSync(realdir);
         let resFiles = [];
         for (let i = 0; i < files.length; i++) {
             let file = files[i];
             let filePath = path.resolve(dir, file);
-            let status = null;
             try {
-                status = fs.lstatSync(filePath);
+                file = getFile(filePath);
+                resFiles.push(file);
             } catch (e) {
-                console.error(e);
-                continue;
+
             }
-            if (status.isFile() || status.isBlockDevice() 
-                || status.isCharacterDevice() || status.isSocket()) {
-                file = {
-                    path: filePath,
-                    name: file,
-                    type: "file",
-                    mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
-                    size: status.size
-                }
-            } else if (status.isSymbolicLink()) {
-                file = {
-                    path: filePath,
-                    name: file,
-                    type: "link",
-                    mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
-                    size: status.size
-                }
-            } else {
-                file = {
-                    path: filePath,
-                    name: file,
-                    type: "dir",
-                    mtime: format.asString('yyyy-MM-dd hh:mm:ss', status.mtime),
-                    size: status.size
-                }
-            }
-            resFiles.push(file);
         }
         return {
             dir: dir,
--- worker.js
+++ worker.js
@@ -2,19 +2,49 @@ const core = require('./core');
 
 self.addEventListener('message', function (e) {
     if (e.data.action === 'ls') {
-        self.postMessage({
-            type: 'ls',
-            data: core.ls(e.data.data)
-        });
+        try {
+            self.postMessage({
+                type: 'ls',
+                data: core.ls(e.data.data)
+            });
+        } catch (e) {
+            self.postMessage({
+                type: 'ls',
+                error: e.message
+            });
+        }
+    } else if (e.data.action === 'rm') {
+        try {
+            let res = core.rm(e.data.data, fpath => {
+                self.postMessage({
+                    type: 'rm',
+                    progress: 'doing',
+                    data: fpath
+                });
+            });
+            self.postMessage({
+                type: 'rm',
+                progress: 'finish',
+                data: res
+            });
+        } catch (e) {
+            self.postMessage({
+                type: 'rm',
+                progress: 'finish',
+                error: e.message
+            });
+        }
     } else if (e.data.action === 'scanDir') {
         let res = core.scanDir(e.data.data, result => {
             self.postMessage({
-                type: 'scanning',
+                type: 'scanDir',
+                progress: 'doing',
                 data: result
             });
         });
         self.postMessage({
-            type: 'scanFinish',
+            type: 'scanDir',
+            progress: 'finish',
             data: res
         });
     } else if (e.data.action === 'stop') {
